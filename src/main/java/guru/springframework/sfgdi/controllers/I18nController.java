29b321c106152d343fa0d74da18ef8ca4a8b530d
ribbotson-343 <rich.ibbotson@gmail.com>
2021-04-07 08:47:58 -0400

    more injection scenarios

--- src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -1,21 +1,19 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class SetterInjectedController {
-    @Autowired
-    @Qualifier("setterGreetingService")
-    private GreetingService greetingService;
+public class I18nController {
 
-    public void setGreetingService(GreetingService greetingService) {
+    private final GreetingService greetingService;
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    public String getGreeting(){
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 }
